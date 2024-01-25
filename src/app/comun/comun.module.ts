461d6d654db450187bea3a5463eb9a1c8b3f52a9
Ibra <brahim.kor@educa.jcyl.es>
2024-01-25 16:06:07 +0200

    creado home

--- src/app/comun/comun.module.ts
+++ src/app/comun/comun.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { HomeComponent } from './home/home.component';
+import { PieComponent } from './pie/pie.component';
 
 
 
 @NgModule({
   declarations: [
     CabeceraComponent,
-    HomeComponent
+    HomeComponent,
+    PieComponent
   ],
   exports: [
     CabeceraComponent,
-    HomeComponent
+    HomeComponent,
+    PieComponent
   ],
   imports: [
     CommonModule,
